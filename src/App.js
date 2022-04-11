cb098ad39210f09d5f82be261909ad923fa61fe5
tianledong <dtllawrence@gmail.com>
2022-04-10 21:52:15 -0400

    add socket io with express session

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
 import './styles.css';
 import Tuiter from "./components/tuiter";
-import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:4000')
 function App() {
   return (
     <Tuiter/>
