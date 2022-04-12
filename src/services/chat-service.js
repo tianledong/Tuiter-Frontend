eeafb05ee765940ccf9e31587dec51ae5c9e3182
tianledong <dtllawrence@gmail.com>
2022-04-11 22:18:11 -0400

    add chat service

---
+++ src/services/chat-service.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:4000"
+
+const api = axios.create({
+    withCredentials: true
+});
+
+/**
+ * Records a user chats a user
+ * @param uid user id
+ * @param uid1 another user id
+ * @return {Promise<AxiosResponse<any>>} a new chat object
+ */
+export const userChatsUser = (uid, uid1) =>
+    api.post(`${USERS_API}/users/${uid}/chats/${uid1}`)
+        .then(chat => chat.data);
+
+/**
+ * Retrieval chat history between two users
+ * @param uid user id
+ * @param uid1 another user id
+ * @return {Promise<AxiosResponse<any>>} a new chat object
+ */
+export const findChatForUsers = (uid, uid1) =>
+    api.get(`${USERS_API}/users/${uid}/chats/${uid1}`)
+        .then(chat => chat.data);
